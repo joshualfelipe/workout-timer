3f2736651dd8fd58f3dced2b1ee9b4150842f12c
joshualfelipe <jlfelipe1@up.edu.ph>
2025-06-08 11:17:09 +0800

    feat: add temporary WIP page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Flex, Layout } from "antd";
 import './App.css'
+import { Content, Footer } from "antd/es/layout/layout";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Flex className="h-screen">
+      <Layout>
+        <Content>
+          <Flex gap="middle" justify="center" align="center" vertical className="h-full">
+            <h1 className="font-bold text-center text-4xl pt-4">This is a Work in Progress project.</h1>
+            <h2 className="text-2xl text-center p-4">Working with you soon!</h2>
+          </Flex>
+        </Content>
+        <Footer>
+          <h1 className="text-l text-center font-bold p-2">© Joshua Felipe 2025</h1>
+        </Footer>
+      </Layout>
+      </Flex>
     </>
   )
 }
